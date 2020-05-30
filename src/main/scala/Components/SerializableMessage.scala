cbcf7eda5c9c4244e3e863b6650e39f1917e7b01
Pinioo <jakpin1999@gmail.com>
2020-05-30 12:28:46 +0200

    Basic room messages added

--- src/main/scala/Components/SerializableMessage.scala
+++ src/main/scala/Components/SerializableMessage.scala
@@ -3,6 +3,10 @@ package Components
 sealed trait SerializableMessage
 
 final case class Signal(info: String) extends SerializableMessage
+final case class Start(u: Unit) extends SerializableMessage
+case object Start{
+  def apply(): Start = Start(())
+}
 
 sealed trait ConnectionMessage extends SerializableMessage
 
@@ -16,7 +20,9 @@ sealed trait ListingMessage extends SerializableMessage
 final case class ListingRequest(name: String) extends ListingMessage
 final case class Listing(logins: List[String]) extends ListingMessage
 
-final case class Start(u: Unit) extends SerializableMessage
-case object Start{
-  def apply(): Start = Start(())
-}
+sealed trait RoomMessage extends SerializableMessage
+
+final case class RoomCreationRequest(name: String) extends RoomMessage
+final case class RoomJoinRequest(name: String, roomId: Int) extends RoomMessage
+final case class RoomCreated(roomId: Int) extends RoomMessage
+final case class RoomJoined(roomId: Int) extends RoomMessage
