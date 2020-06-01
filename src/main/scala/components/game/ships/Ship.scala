ca7aa1112ffdb48102b2722f0e6c9aa76d5cf11f
marcinwasowicz <marcinkwasowicz@gmail.com>
2020-06-01 22:58:13 +0200

    Fixed taking damage by ship

--- src/main/scala/components/game/ships/Ship.scala
+++ src/main/scala/components/game/ships/Ship.scala
@@ -3,6 +3,7 @@ package components.game.ships
 class Ship(var healthPoints: Int) {
   def getDamage() = {
     healthPoints = healthPoints - 1
+    healthPoints = scala.math.min(healthPoints, 0)
   }
 
   def isSunk(): Boolean = {
