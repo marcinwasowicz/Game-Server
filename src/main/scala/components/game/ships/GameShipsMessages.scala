d64d16303d3b3574bb0f6985fb504214de8527a8
marcinwasowicz <marcinkwasowicz@gmail.com>
2020-06-01 19:14:10 +0200

    Init Board Message

--- src/main/scala/components/game/ships/GameShipsMessages.scala
+++ src/main/scala/components/game/ships/GameShipsMessages.scala
@@ -2,7 +2,7 @@ package components.game.ships
 
 import components.common.GameMessage
 
-final case class InitBoardMessage(positions: List[(Int, Int)], isFirst: Boolean) extends GameMessage
+final case class InitBoardMessage(gridSize: (Int, Int), positions: List[(Int, Int)], isFirst: Boolean) extends GameMessage
 final case class ShotRequestMessage(target: (Int, Int)) extends GameMessage
 final case class ShotResultMessage(target: (Int, Int), shooter: String,  hit: Boolean, sunk: Boolean) extends GameMessage
 final case class GameEndMessage(winner: String) extends GameMessage
--- src/main/scala/components/game/ships/GameShipsServer.scala
+++ src/main/scala/components/game/ships/GameShipsServer.scala
@@ -2,15 +2,35 @@ package components.game.ships
 
 import akka.actor.ActorRef
 
+
 import scala.collection.mutable
 
 class GameShipsServer(playersInRoom: mutable.HashMap[String, ActorRef]) extends GameShips {
 
-  val firstPlayerBoard = new mutable.HashMap[(Int, Int), Ship]()
-  val secondPlayerBoard = new mutable.HashMap[(Int, Int), Ship]()
+  val width = 10
+  val height= 10
+
+  var clientsBoardDictionary = new mutable.HashMap[String, mutable.HashMap[(Int, Int), Ship]]()
+  var gameClientDictionary = new mutable.HashMap[String, ActorRef]()
+  var counter = 0
+
+  def initializeBoard(board: mutable.HashMap[(Int, Int), Ship]) = {}
+
+  def initializeAndSendBoards() = gameClientDictionary.foreach{case (name, ref) =>
+    initializeBoard(clientsBoardDictionary.apply(name))
+
+  }
+
 
 
   override def receive: Receive = {
+    case GameClientCreated(name) =>
+      gameClientDictionary.put(name, sender())
+      clientsBoardDictionary.put(name, new mutable.HashMap[(Int, Int), Ship]())
+      counter += 1
+      if(counter >= 2){
+        initializeAndSendBoards()
+      }
     case
   }
 }
