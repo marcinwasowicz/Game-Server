3b588476762df2c19cf79a4410b6439cd2f1db68
marcinwasowicz <marcinkwasowicz@gmail.com>
2020-06-01 19:47:23 +0200

    Try Shooting Implemented

--- src/main/scala/components/game/ships/GameShipsServer.scala
+++ src/main/scala/components/game/ships/GameShipsServer.scala
@@ -6,21 +6,40 @@ import akka.actor.ActorRef
 import scala.collection.mutable
 
 class GameShipsServer(playersInRoom: mutable.HashMap[String, ActorRef]) extends GameShips {
+  val hitFailed = false
+  val hitSuccess = true
+  val sinkFailed = false
+  val sinkSuccess = true
 
   val width = 10
   val height= 10
+  val numberOfShips = 10
 
   var clientsBoardDictionary = new mutable.HashMap[String, mutable.HashMap[(Int, Int), Ship]]()
   var gameClientDictionary = new mutable.HashMap[String, ActorRef]()
   var counter = 0
 
-  def initializeBoard(board: mutable.HashMap[(Int, Int), Ship]) = {}
+  def initializeBoard(board: mutable.HashMap[(Int, Int), Ship]): Unit = {}
 
-  def initializeAndSendBoards() = gameClientDictionary.foreach{case (name, ref) =>
-    initializeBoard(clientsBoardDictionary.apply(name))
+  def tryShooting(target: (Int, Int), shooter: ActorRef): Unit = {
+    val shooterName: String = gameClientDictionary.filter(pair => pair._2 == shooter).keys.head
+    val targetName: String = gameClientDictionary.filter(pair => pair._2 != shooter).keys.head
 
+    if (clientsBoardDictionary.apply(targetName).contains(target)) {
+      clientsBoardDictionary.apply(targetName).apply(target).getDamage()
+      gameClientDictionary.foreach { case (_, ref) => ref ! ShotResultMessage(target, shooterName, hitSuccess,
+        clientsBoardDictionary.apply(targetName).apply(target).isSunk())
+      }
+    } else {
+      gameClientDictionary.foreach { case (_, ref) => ref ! ShotResultMessage(target, shooterName, hitFailed, sinkFailed) }
+    }
   }
 
+
+  def initializeAndSendBoards(): Unit = gameClientDictionary.foreach{case (name, ref) =>
+    initializeBoard(clientsBoardDictionary.apply(name))
+    ref ! InitBoardMessage((width, height),clientsBoardDictionary.apply(name).keysIterator.toList, false )
+  }
 
 
   override def receive: Receive = {
@@ -31,6 +50,7 @@ class GameShipsServer(playersInRoom: mutable.HashMap[String, ActorRef]) extends 
       if(counter >= 2){
         initializeAndSendBoards()
       }
-    case
+    case ShotRequestMessage(target) =>
+      tryShooting(target, sender())
   }
 }
