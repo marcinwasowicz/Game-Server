b9341e0770ac747dd41234064785b16f76f09e08
marcinwasowicz <marcinkwasowicz@gmail.com>
2020-06-02 12:02:38 +0200

    Polishing GameShipsServer

--- src/main/scala/components/game/ships/GameShipsServer.scala
+++ src/main/scala/components/game/ships/GameShipsServer.scala
@@ -11,10 +11,9 @@ class GameShipsServer() extends GameShips {
   val numberOfShips = 1
   val shipLength = 3
 
-  var clientsBoardDictionary = new mutable.HashMap[String, mutable.HashMap[(Int, Int), Ship]]()
-  var gameClientDictionary = new mutable.HashMap[String, ActorRef]()
-  var counter = 0
-  var isFirst = true
+  var clientBoards = new mutable.HashMap[String, mutable.HashMap[(Int, Int), Ship]]()
+  var clientDictionary = new mutable.HashMap[String, ActorRef]()
+  var whoIsFirst = new mutable.HashMap[String, Boolean]()
 
   def checkRow(position: (Int, Int), diff: (Int, Int), length: Int, dict: mutable.HashMap[(Int, Int), Ship]): Boolean =
     (0 until length).toList.forall(num => !dict.contains((position._1 + num*diff._1, position._2+num*diff._2)))
@@ -28,61 +27,65 @@ class GameShipsServer() extends GameShips {
     position._2+(length-1)*direction._2 >=0 && position._2+(length-1)*direction._2 < width
 
   def tryPosition(position: (Int, Int), length: Int, dict: mutable.HashMap[(Int, Int), Ship]): Boolean = {
-    val directions = List((-1, 0), (1, 0), (0, 1), (0, -1)).filter(direction => checkBorders(position, length, direction))
-    directions.foldLeft(false)((acc, direction) =>
-      if(!acc && checkRow(position, direction, length, dict)){
-        fillRow(position, direction, length, dict, new Ship(length))
-        !acc
-      } else acc)
+    List((-1, 0), (1, 0), (0, 1), (0, -1)).
+      filter(direction => checkBorders(position, length, direction)).
+        foldLeft(false)((acc, direction) =>
+          if(!acc && checkRow(position, direction, length, dict)){
+            fillRow(position, direction, length, dict, new Ship(length))
+            !acc
+          } else acc)
   }
 
-  def initializeBoard(board: mutable.HashMap[(Int, Int), Ship]): Unit = {
-    val randomGenerator = Random
-    for(_<- 1 to numberOfShips){
-      var position = (randomGenerator.nextInt(height), randomGenerator.nextInt(width))
-      while(!tryPosition(position, shipLength, board)){
-        position = (randomGenerator.nextInt(height), randomGenerator.nextInt(width))
-      }
-    }
+  def findPosition(length: Int, board: mutable.HashMap[(Int, Int), Ship]): Unit =
+    while(!tryPosition((Random.nextInt(height), Random.nextInt(width)), length, board)){}
+
+  def initializeBoard(board: mutable.HashMap[(Int, Int), Ship], numOfShips: Int, length: Int): Unit =
+    (1 to numOfShips).foreach( _ => findPosition(length, board))
+
+  def sendBoard(keyValPair: (String, ActorRef)): Unit ={
+    keyValPair._2 ! InitBoardMessage((height, width), clientBoards.apply(keyValPair._1).keys.toList,whoIsFirst.apply(keyValPair._1))
   }
 
-  def incrementAndCheckClients(): Unit = {
-    counter = counter + 1
-    if(counter >= 2){
+  def initializeAndSendBoards(): Unit = {
+    clientBoards.foreach(keyValPair => initializeBoard(clientBoards.apply(keyValPair._1), numberOfShips, shipLength))
+    clientDictionary.foreach(keyValPair => sendBoard(keyValPair))
+  }
+
+  def incrementAndCheckClients(name: String, ref: ActorRef): Unit = {
+    clientDictionary.put(name, ref)
+    clientBoards.put(name, new mutable.HashMap[(Int, Int), Ship]())
+    if(clientDictionary.size >= 2){
+      whoIsFirst.put(name, false)
       initializeAndSendBoards()
+    }
+    else{
+      whoIsFirst.put(name, true)
     }
   }
 
   def tryShooting(target: (Int, Int), shooter: ActorRef): Unit = {
-    val shooterName: String = gameClientDictionary.filter(pair => pair._2 == shooter).keys.head
-    val targetName: String = gameClientDictionary.filter(pair => pair._2 != shooter).keys.head
+    val shooterName: String = clientDictionary.filter(pair => pair._2 == shooter).keys.head
+    val targetName: String = clientDictionary.filter(pair => pair._2 != shooter).keys.head
+    val targetBoard = clientBoards.apply(targetName)
 
-    if (clientsBoardDictionary.apply(targetName).contains(target)) {
-      clientsBoardDictionary.apply(targetName).apply(target).getDamage()
-      if(clientsBoardDictionary.apply(targetName).values.toList.forall(ship => ship.isSunk())){
-        gameClientDictionary.foreach{case(_, ref) => ref ! GameEndMessage(shooterName)}
+    if (targetBoard.contains(target)) {
+      val targetShip = targetBoard.apply(target)
+      targetShip.getDamage()
+      if(targetBoard.values.toList.forall(ship => ship.isSunk())){
+        clientDictionary.values.foreach(ref => ref ! GameEndMessage(shooterName))
       }
       else {
-        gameClientDictionary.foreach { case (_, ref) => ref ! ShotResultMessage(target, shooterName, true,
-          clientsBoardDictionary.apply(targetName).apply(target).isSunk())
-        }
+        clientDictionary.values.foreach(ref => ref ! ShotResultMessage(target, shooterName, true,targetShip.isSunk()))
       }
-    } else {
-      gameClientDictionary.foreach { case (_, ref) => ref ! ShotResultMessage(target, shooterName, false, false) }
     }
-  }
-
-  def initializeAndSendBoards(): Unit = gameClientDictionary.foreach{case (name, ref) =>
-    initializeBoard(clientsBoardDictionary.apply(name))
-    ref ! InitBoardMessage((width, height),clientsBoardDictionary.apply(name).keys.toList, isFirst)
-    isFirst = !isFirst
+    else {
+      clientDictionary.values.foreach(ref => ref ! ShotResultMessage(target, shooterName, false, false))
+    }
   }
 
   override def receive: Receive = {
     case GameClientCreated(name) =>
-      gameClientDictionary.put(name, sender())
-      clientsBoardDictionary.put(name, new mutable.HashMap[(Int, Int), Ship]())
-      incrementAndCheckClients()
+      incrementAndCheckClients(name, sender())
     case ShotRequestMessage(target) =>
       tryShooting(target, sender())
   }
