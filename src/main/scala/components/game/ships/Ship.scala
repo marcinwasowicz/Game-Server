7508c131cb0d36cf659e17730e4ddac169169fda
marcinwasowicz <marcinkwasowicz@gmail.com>
2020-06-02 12:04:55 +0200

    Ship class simplified

--- src/main/scala/components/game/ships/Ship.scala
+++ src/main/scala/components/game/ships/Ship.scala
@@ -3,10 +3,9 @@ package components.game.ships
 class Ship(var healthPoints: Int) {
   def getDamage() = {
     healthPoints = healthPoints - 1
-    healthPoints = scala.math.max(healthPoints, 0)
   }
 
   def isSunk(): Boolean = {
-    healthPoints == 0
+    healthPoints <= 0
   }
 }
